26648404b0888c341442c8544c8e31842d885467
Shi Weiwen <myheartsgoon@gmail.com>
2017-06-17 22:57:42 +0800

    Add map and some features

--- routes.py
+++ routes.py
@@ -1,6 +1,9 @@
-from flask import Flask, render_template, request, redirect, session, url_for
+# coding=utf-8
+from flask import Flask, render_template, request, redirect, session, url_for, flash
 from models import db, User
 from forms import SignupForm, LoginForm, AddressForm
+
+
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:62171175110@127.0.0.1/flask_web'
@@ -24,6 +27,11 @@ def signup():
     form = SignupForm()
     if request.method == 'POST':
         if not form.validate():
+            return render_template("signup.html", form=form)
+        email = form.email.data
+        user = User.query.filter_by(email=email).first()
+        if user is not None:
+            flash("Email has been used, please use another one!")
             return render_template("signup.html", form=form)
         else:
             newuser = User(form.first_name.data, form.last_name.data, form.email.data, form.password.data)
@@ -51,7 +59,8 @@ def login():
                 session['email'] = email
                 return redirect(url_for('home'))
             else:
-                return redirect(url_for('login'))
+                flash('Email address or password incorrect!')
+                return render_template('login.html', form=form)
     elif request.method == 'GET':
         return render_template('login.html', form=form)
 
@@ -72,6 +81,14 @@ def home():
             pass
     elif request.method == 'GET':
         return render_template('home.html', form=form)
+
+@app.route('/map')
+def map():
+    return render_template('map.html')
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('404.html'), 404
 
 if __name__ == "__main__":
     app.run('127.0.0.1', port=5000, debug=True)
