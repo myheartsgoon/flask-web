7fa20d78d2b52e3f916f82ddb8667c1b0845f910
Shi Weiwen <myheartsgoon@gmail.com>
2017-06-24 15:09:28 +0800

    test

--- routes.py
+++ routes.py
@@ -124,4 +124,4 @@ def page_not_found(error):
     return render_template('404.html'), 404
 
 if __name__ == "__main__":
-    app.run('0.0.0.0', port=5000, debug=True)
+    app.run('0.0.0.0', port=5000)
