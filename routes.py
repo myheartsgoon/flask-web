eaa28ba5a25f404034ccae24371820ed443dfdbb
Shi Weiwen <myheartsgoon@gmail.com>
2017-06-27 14:06:51 +0800

    Modify DB var

--- routes.py
+++ routes.py
@@ -9,7 +9,7 @@ import os
 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = app.config.from_envvar('DATABASE_URL')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 
 
 db.init_app(app)
